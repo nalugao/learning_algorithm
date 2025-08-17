78d3a03d12f521aebe08501931e2a15482f8a2c4
Marco <mgrazianodecastro@gmail.com>
2025-08-17 11:42:10 -0300

    structs
    Data structs to be implemented for queue and stack

---
+++ structs/abstract.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class Collection(ABC):
+
+    def __init__(self):
+        self._items = []
+
+    @abstractmethod
+    def add(self, item):
+        """Add an item to the collection."""
+        pass
+
+    @abstractmethod
+    def pop(self):
+        """Remove and return the item from the collection."""
+        pass
+
+    def __bool__(self):
+        """Check if the collection is not empty."""
+        return bool(self._items)
